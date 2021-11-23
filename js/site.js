edfefed7c19e9fa4867edc7fd0a2ebff73030045
DENRIG777 <62824680+DENRIG777@users.noreply.github.com>
2021-11-23 01:02:02 -0600

    added content

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-// Call Hello World
-function HelloWorld() {
-    alert("hello World");
+//get values form the user. we need to get the Fizz and the buzz value
+function getValues (){
+
+    //get the user values from the page
+
+    //parse for numbers
+
+    //check that the numbers are integers
+
+    //we call fizzbuzz 
+
+    //call displayData and write the values to the screen
+
+}
+
+
+function fizzBuzz(fizzValue, buzzValue){
+
+    //init the returnArray
+
+    //
+
+
+
 }
